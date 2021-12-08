4025fded3835e59ee65f4edaca1e4f395496ddf7
zeinabkamel <47484937+zeinabkamel@users.noreply.github.com>
2021-12-09 01:23:15 +0200

    finish

--- src/Compoents/UI/Buttons/button.js
+++ src/Compoents/UI/Buttons/button.js
@@ -2,8 +2,13 @@ import react from "react";
 import styles from "./button.css";
 
 const Button = (props) => {
+  console.log("Sf", styles);
   return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
+    <button
+      type={props.type}
+      className={styles["button"]}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
